fca784815177e9e984132c8aaf5b854819e0d449
nuraly <nuraly.zhanbyrbayev@gmail.com>
2020-09-01 21:57:29 +0600

    Add empty django project.

---
+++ memento/memento/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for memento project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'memento.settings')
+
+application = get_asgi_application()
