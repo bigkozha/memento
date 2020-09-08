3c7999908c244040c2fadb1bc5bcd9dc4a44c401
nuraly <nuraly.zhanbyrbayev@gmail.com>
2020-09-08 11:41:33 +0600

    Temp commit.

--- memento/game/consumers.py
+++ memento/game/consumers.py
@@ -1,46 +1,57 @@
 import json
-from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
+class ChatConsumer(AsyncWebsocketConsumer):
+    
+    tile_won  = []
+
+    async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
-
+        self.items = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5]
         # Join room group
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
 
-        self.accept()
+        await self.accept()
+        await self.send(text_data=json.dumps({
+            'items': self.items,
+            'tile_won': ChatConsumer.tile_won,
+        }))
 
-    def disconnect(self, close_code):
+    async def disconnect(self, close_code):
         # Leave room group
-        async_to_sync(self.channel_layer.group_discard)(
+        await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
 
     # Receive message from WebSocket
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-
+        tile_opened = []
+        if 'tileOpened' in text_data_json:
+            tile_opened = text_data_json['tileOpened']
+        if 'tile_won' in text_data_json:
+            ChatConsumer.tile_won = text_data_json['tile_won']
         # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(
+        await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'tile_opened': tile_opened,
             }
         )
 
     # Receive message from room group
-    def chat_message(self, event):
-        message = event['message']
-
+    async def chat_message(self, event):
+        if 'tile_opened' in event:
+            tile_opened = event['tile_opened']
+        else:
+            tile_opened = ''
         # Send message to WebSocket
-        self.send(text_data=json.dumps({
-            'message': message
+        await self.send(text_data=json.dumps({
+            'tile_opened': tile_opened,
         }))
